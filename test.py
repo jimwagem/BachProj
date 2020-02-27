6b5bed3308ad91e8c3a8f7c2802c4902b7ee5b1a
Jim Wagemans <jimwagemans@gmail.com>
2020-02-27 15:54:33 +0100

    First version reconstruction

--- test.py
+++ test.py
@@ -7,6 +7,8 @@ import glob
 
 def read1dtiff(filename):
     """Returns a 1d array"""
+    if isinstance(filename,list):
+        filename = filename[0]
     im = skimage.io.imread(filename)
     return im[0]
 
@@ -20,9 +22,12 @@ def averageField(filenames,size):
     return result_field
 
 # Data and Distance from source to object/detector respectively.
-data_dir = "/JimsBreakfast/TestScan_06-02-20_fakecoffee_highres/"
-SOD = 423.560547
+data_dir = "./JimsBreakfast/TestScan_06-02-20_creamer/"
+SOD = 203.549805
 SDD = 528.014648
+
+# CPU or GPU
+useCPU = True
 
 # Preprocessing
 binning = 1
@@ -67,27 +72,68 @@ SDD *= scale_fac
 SOD *= scale_fac
 dec_pixel_size = binning * 0.074800 * scale_fac
 
-proj_geo = astra.create_proj_geom('fanflat', dec_pixel_size, detector_size[1], angles, SOD, SDD - SOD)
+proj_geom = astra.create_proj_geom('fanflat', dec_pixel_size, detector_size, angles, SOD, SDD - SOD)
 
 # Reading and preprocessing data
-dark_field = averageField(dark_field_files)
-flat_field = averageField(light_field_files)
+dark_field = averageField(dark_field_files, detector_size)
+flat_field = averageField(light_field_files, detector_size)
 
 # Read data
-data = np.zeros(projection_files)
+data = np.zeros([n_pro, detector_size])
 dark_light_diff = (flat_field - dark_field)
 if 0 in dark_light_diff:
     print("Division by 0")
     exit
 
-for data_file in projection_files:
-    data_temp = np.array(read1dtiff(data_file))
-    data_temp.astype(float)
-    data_temp = np.divide((data_temp - dark_field),dark_light_diff)
-    data += data_temp
-print(data)
-# # Reading files
-# im = skimage.io.imread('test.tif')
-# plt.plot(im[0])
+for index, data_file in enumerate(projection_files):
+    data_partial = np.array(read1dtiff(data_file))
+    data_partial.astype(float)
+    data_partial = np.divide((data_partial - dark_field),dark_light_diff)
+    data[index] = data_partial
+
+# Set all values smaller than 0 to the minimum (of values greater than 0)
+# Also clip the values to 1.
+min_data = min([elem for elem in data.flatten() if elem > 0])
+data = [np.clip(sublist, min_data, 1) for sublist in data]
+
+# Apply logarithm
+data = np.array([-np.log(sublist) for sublist in data])
+
+# Display sinogram
+# plt.imshow(data)
 # plt.show()
+# Create astra object for reconstruction
+recID = astra.data2d.create('-vol', vol_geom)
+sinoID = astra.data2d.create('-sino', proj_geom, data)
+
+if useCPU:
+    proj_id = astra.create_projector('line_fanflat', proj_geom, vol_geom)
+
+    # Create configuration
+    cfg = astra.astra_dict('SIRT')
+    cfg['ReconstructionDataId'] = recID
+    cfg['ProjectionDataId'] = sinoID
+    cfg['ProjectorId'] = proj_id
+else:
+    #TODO implement GPU
+    pass
+
+# Create and run algorithm
+alg_id = astra.algorithm.create(cfg)
+iterations = 5
+astra.algorithm.run(alg_id, iterations)
+
+# Recieve reconstruction
+rec = astra.data2d.get(recID)
+plt.imshow(10*rec)
+plt.show()
+plt.imshow(100*rec)
+plt.show()
+
+# Clean up.
+astra.algorithm.delete(alg_id)
+astra.data2d.delete(recID)
+astra.data2d.delete(sinoID)
+if useCPU:
+    astra.projector.delete(proj_id)
 
